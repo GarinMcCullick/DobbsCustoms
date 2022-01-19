57f1afefb98bd0ca97880adc35b50392d0226fa0
Garin Mccullick <glmccullick@gmail.com>
2022-01-19 02:25:42 -0600

    added some shadows

--- dobbs-customs/src/Components/Gallery.js
+++ dobbs-customs/src/Components/Gallery.js
@@ -20,18 +20,23 @@ const Container = styled.div`
 const InnerContainer = styled.div`
     width:95%;
     height:95%;
-    border:1px solid black;
+    background-color:lightgrey;
     display:flex;
     flex-flow:row wrap;
     justify-content:space-evenly;
     align-items:start;
+    border-radius:25px;
+    z-index:1;
+    box-shadow: 0 19px 38px rgba(0,0,0,0.10), 0 15px 12px rgba(0,0,0,0.20);
 `
 
 const ImgContainers = styled.div`
     width:400px;
     height:350px;
     border:1px solid black;
+    background-color:white;
     margin-top:25px;
+    z-index:2;
 `
 
 export default function Gallery() {
